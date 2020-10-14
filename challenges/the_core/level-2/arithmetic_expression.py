50438d0190127a2c03e03c5452e1b8075e03097f
Tim McCabe <timmccabe44@gmail.com>
2020-10-13 21:43:16 -0400

    arithmetic expression

---
+++ challenges/the_core/level-2/arithmetic_expression.py
@@ -0,0 +1,10 @@
+def arithmeticExpression(a, b, c):
+    if a + b == c:
+        return True
+    if a - b == c:
+        return True
+    if a * b == c:
+        return True
+    if a / b == c:
+        return True
+    return False
